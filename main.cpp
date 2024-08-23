c0553086a3650796f421ab26940d53b55e573b6d
Marcos7502 <mgomezv@fi.uba.ar>
2024-08-23 00:39:34 +0000

    Style: Added comments

--- main.cpp
+++ main.cpp
@@ -1,20 +1,22 @@
+/* @autores: Marcos Gomez Villafañe y Hernan Leandro Bailo 
+ * @fecha: 22/8/2024 */
 #include "mbed.h"
 #include "arm_book_lib.h"
 
+//=====[Funcion principal, punto de entrada despues de encendido o reset]========
 int main()
 {
-    DigitalIn gasDetector(D2);
+    DigitalIn gasDetector(D2); /**< Objeto asociado al detector de gas (D2) */
 
-    DigitalOut alarmLed(LED1);
+    DigitalOut alarmLed(LED1); /**< Salida de la alarma en led LD1*/
 
-    gasDetector.mode(PullDown);
+    gasDetector.mode(PullDown); /**< Modo de detección del gas detector*/
 
     alarmLed = OFF;
 
-    
-
+    //=====[Loop de funcionamiento, programa que se ejecuta constantemente]========                                                                               
     while (true) {
-        printf("%s\n","Hello World");
+        printf("%s\n","Hello World"); 
 
         if ( gasDetector == ON ) {
             alarmLed = ON;
