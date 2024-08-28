5c0f203a8e72d17bbf5ffb238e8a70f0f4d9c7bd
Hernán Leandro Bailo <31524092+hbailo@users.noreply.github.com>
2024-08-28 00:26:47 -0300

    Add doxygen support

--- main.cpp
+++ main.cpp
@@ -1,29 +1,30 @@
-/* @autores: Marcos Gomez Villafañe y Hernan Leandro Bailo 
- * @fecha: 22/8/2024 */
+/**
+ * @file
+ */
+
 #include "mbed.h"
 #include "arm_book_lib.h"
 
-//=====[Funcion principal, punto de entrada despues de encendido o reset]========
 int main()
 {
-    DigitalIn gasDetector(D2); /**< Objeto asociado al detector de gas (D2) */
 
-    DigitalOut alarmLed(LED1); /**< Salida de la alarma en led LD1*/
+  DigitalIn gasDetector(D2);
 
-    gasDetector.mode(PullDown); /**< Modo de detección del gas detector*/
+  DigitalOut alarmLed(LED1);
 
-    alarmLed = OFF;
+  gasDetector.mode(PullDown);
 
-    //=====[Loop de funcionamiento, programa que se ejecuta constantemente]========                                                                               
-    while (true) {
-        printf("%s\n","Hello World"); 
+  alarmLed = OFF;
 
-        if ( gasDetector == ON ) {
-            alarmLed = ON;
-        }
-        
-        if ( gasDetector == OFF ) {
-            alarmLed = OFF;
-        }
+  while (true) {
+    printf("%s\n","Hello World"); 
+
+    if ( gasDetector == ON ) {
+      alarmLed = ON;
     }
+        
+    if ( gasDetector == OFF ) {
+      alarmLed = OFF;
+    }
+  }
 }
